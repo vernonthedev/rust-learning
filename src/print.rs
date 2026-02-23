0486185995e5e9f829d2c5518538da14e12c7464
vernonthedev <techjaja2@gmail.com>
2026-02-23 12:09:47 +0300

    chore: added constants namings setup from rust docs

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,20 @@
 pub fn run() {
     println!("Print from the print.rs file");
+
+    let name = "Brother";
+    let mut age = 28;
+
+    println!("{} is {} old.", name, age);
+    age = 45;
+    println!("{} is {} old.", name, age);
+
+    /**
+     * Defining Constants.
+     */
+    const MAIN_CONSTANT: i32 = 001;
+    println!("{} is my constant", MAIN_CONSTANT);
+
+    // Assigning multiple vars.
+    let (my_name, my_age) = ("Vernon", 21);
+    println!("{} is now {} old.", my_name, my_age);
 }
