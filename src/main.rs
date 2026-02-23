844a1ab7315d01ee96d27c97e297dcca7875cb6b
vernonthedev <techjaja2@gmail.com>
2026-02-23 11:53:33 +0300

    chore: initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+mod print;
+
+fn main() {
+    println!("Hello, world!");
+    print::run();
+    println!("Number: {}", 1);
+    println!("{} is from {}", "Vernon", "Miami");
+    println!("{0} is from {1} and {1} is great", "Vernon", "Miami");
+
+    println!("{} is liked by {name}", "Coding", name = "vernonthedev");
+
+    println!(
+        "{name} likes {action}",
+        name = "vernonthedev",
+        action = "Coding"
+    );
+
+    println!("Binary: {:b} HEX: {:x} Octal: {:o}", 10, 10, 10);
+
+    // Debug Trait
+    println!("{:?}", (12, "Hey", true));
+
+    println!("10 + 10 = {}", 10 + 10);
+}
---
+++ src/print.rs
@@ -0,0 +1,3 @@
+pub fn run() {
+    println!("Print from the print.rs file");
+}
